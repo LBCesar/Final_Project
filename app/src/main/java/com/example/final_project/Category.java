c31c0bcde137b76b4a26363d97558e55981f87e1
Shoraj Manandhar <shorajmanandhar@hotmail.com>
2020-06-27 17:40:28 -0700

    Basic Layout Structure Added

---
+++ app/src/main/java/com/example/final_project/Category.java
@@ -0,0 +1,23 @@
+package com.example.final_project;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class Category extends Fragment {
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.tab2_category, container, false);
+
+        return root;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/final_project/Dashboard.java
@@ -0,0 +1,27 @@
+package com.example.final_project;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+
+// tab1 dashboard
+public class Dashboard extends Fragment {
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.tab1_dashboard, container, false);
+
+        return root;
+    }
+
+}
