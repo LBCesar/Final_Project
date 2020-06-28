39362a543b83d907b8f9d0863c3783fe6b55d320
Shoraj Manandhar <shorajmanandhar@hotmail.com>
2020-06-27 21:27:56 -0700

    First Useless Pie-chart Graph Added

--- app/src/main/java/com/example/final_project/Dashboard.java
+++ app/src/main/java/com/example/final_project/Dashboard.java
@@ -12,8 +12,21 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // tab1 dashboard
 public class Dashboard extends Fragment {
+
+//    AnyChartView anyChartView = getView().getRootView().findViewById(R.id.piechart);
 
     @Override
     public View onCreateView(
@@ -21,7 +34,33 @@ public class Dashboard extends Fragment {
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.tab1_dashboard, container, false);
 
+
+        // From github code example
+        AnyChartView anyChartView = root.findViewById(R.id.piechart);
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("MSI GE75 GAMING", 1300));
+        pie.data(data);
+        pie.labels().position("outside");
+
+        pie.legend().title().enabled(true);
+        pie.legend().title()
+                .text("My yearly expense")
+                .padding(0d, 0d, 10d, 0d);
+
+        pie.legend()
+                .position("center-bottom")
+                .itemsLayout(LegendLayout.HORIZONTAL)
+                .align(Align.CENTER);
+
+        anyChartView.setChart(pie);
+
+
         return root;
     }
+
+
+
 
 }
